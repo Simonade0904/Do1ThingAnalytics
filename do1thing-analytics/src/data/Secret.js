933daba89a5d28c6788fb0359c31c9c91ff2a941
QW <wangqy@umich.edu>
2023-04-16 14:28:22 -0400

    Initialize web app

---
+++ do1thing-analytics/src/data/Secret.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCMyqWlFFXxK2JOmjtT1ef_00_6Wau0l3U",
+    authDomain: "do1thing-3be9d.firebaseapp.com",
+    projectId: "do1thing-3be9d",
+    storageBucket: "do1thing-3be9d.appspot.com",
+    messagingSenderId: "580201853625",
+    appId: "1:580201853625:web:d96171c590c7cdebcbe168",
+    measurementId: "G-N3XW8013R2"
+  };
+
+export default firebaseConfig;
